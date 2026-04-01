f42b42f50a5fcb20ef39827535e30391938e4337
singto1597-cs-smu <singto1597@gmail.com>
2026-04-01 16:58:37 +0700

    1-4-69

--- src/2_incamp/1_cmu-grader/C2P44/C2P44.cpp
+++ src/2_incamp/1_cmu-grader/C2P44/C2P44.cpp
@@ -1,7 +1,50 @@
 #include <bits/stdc++.h>
 
+long long dp[60] = {};
+
+
+
+long long recur(long long n){
+    if (n == 0 || n == 1){
+        dp[n] = 1;
+        return 1;
+    }
+    if (n < 0){
+        dp[n] = 0;
+        return 0;
+    }
+
+    if (dp[n] != 0){
+        return dp[n];
+    }
+
+    dp[n] = recur(n - 1) + recur(n - 3) + recur(n - 4);
+
+    return dp[n];
+}
+
+long long bottom_up(long long n){
+    long long array[n+5] = {};
+    array[0] = 1;
+    array[1] = 1;
+    array[2] = 1;
+    array[3] = 2;
+    array[4] = 4;
+    for (int i = 5; i <= n; i++){
+        array[i] = array[i-1] + array[i-3] + array[i-4];
+    }
+    return array[n];
+}
+
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+    // cout << recur(n) << endl;
+    cout << bottom_up(n) << endl;
 
+    // for (int i = 0; i<= n; i++){
+    //     cout << dp[i] << " ";
+    // }
 }
--- src/2_incamp/1_cmu-grader/C2T06/C2T06.cpp
+++ src/2_incamp/1_cmu-grader/C2T06/C2T06.cpp
@@ -2,6 +2,33 @@
 
 using namespace std;
 
-int main(){
+long long dp[1000005] = {};
 
+long long k = 0;
+
+long long recur(long long n){
+    if (n == 1 || n == 0) return 1;
+    
+    if (dp[n] != 0) return dp[n];
+
+    long long ans = 0;
+
+    for (long long i = 1; i <= k; i++){
+        if (n - i >= 0){
+            ans = (ans + recur(n - i)) % 1000000007;
+
+        }
+    }
+
+    dp[n] = ans;
+
+    return dp[n];
+}
+
+
+int main(){
+    long long n;
+    cin >> n >> k;
+
+    cout << recur(n) << endl;
 }
