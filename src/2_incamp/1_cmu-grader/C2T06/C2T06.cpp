5e860ae358a19676fa2c9b620cd70096a41e4bbd
singto1597 <singto1597@gmail.com>
2026-04-01 21:35:31 +0700

    c2t6 no prefix sum

--- src/2_incamp/1_cmu-grader/C2T06/C2T06.cpp
+++ src/2_incamp/1_cmu-grader/C2T06/C2T06.cpp
@@ -7,8 +7,11 @@ long long dp[1000005] = {};
 long long k = 0;
 
 long long recur(long long n){
-    if (n == 1 || n == 0) return 1;
+    if (n == 1 || n == 0){
+        dp[n] = 1;
+        return 1;
     
+    }
     if (dp[n] != 0) return dp[n];
 
     long long ans = 0;
@@ -25,10 +28,32 @@ long long recur(long long n){
     return dp[n];
 }
 
+long long bottom_up(long long n){
+    vector<long long> array(n + 5, 0);
+    array[0] = 1;
+    array[1] = 1;
+
+    for (long long i = 2; i <= n; i++){
+        for (long long j = 1; j <= k; j++){
+            if (i - j < 0) continue;
+            array[i] = (array[i] + array[i - j]) % 1000000007;
+        }
+    }
+    //for (int i = 0; i<= n; i++){
+    //    cout << array[i] << " ";
+    //}
+    return array[n];
+}
+
 
 int main(){
     long long n;
     cin >> n >> k;
 
-    cout << recur(n) << endl;
+    // cout << recur(n) << endl;
+    cout << bottom_up(n) << endl;
+    //for (int i = 0; i<= n; i++){
+    //    cout << dp[i] << " ";
+    //}
+
 }
