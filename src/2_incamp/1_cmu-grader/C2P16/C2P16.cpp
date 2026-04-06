ae5e14d9b9db66312a45118588ac2c3957098269
singto1597-cs-smu <singto1597@gmail.com>
2026-04-06 13:24:58 +0700

    q01 and p16

--- src/2_incamp/1_cmu-grader/C2P16/C2P16.cpp
+++ src/2_incamp/1_cmu-grader/C2P16/C2P16.cpp
@@ -1,7 +1,46 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(){
+void solve() {
+    int t, p;
+    if (!(cin >> t >> p)) return;
 
+    while (t--) {
+        vector<int> a(p);
+        for (int i = 0; i < p; i++) {
+            cin >> a[i];
+        }
+
+        int flips = 0;
+        
+        for (int bottom = 0; bottom < p; bottom++) {
+            
+            int max_idx = bottom;
+            for (int i = bottom + 1; i < p; i++) {
+                if (a[i] > a[max_idx]) {
+                    max_idx = i;
+                }
+            }
+
+            if (max_idx != bottom) {
+                if (max_idx != p - 1) {
+                    reverse(a.begin() + max_idx, a.end());
+                    flips++;
+                }
+
+                reverse(a.begin() + bottom, a.end());
+                flips++;
+            }
+        }
+        
+        cout << flips << "\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    solve();
+    return 0;
 }
--- src/2_incamp/1_cmu-grader/C2Q001/C2Q001.cpp
+++ src/2_incamp/1_cmu-grader/C2Q001/C2Q001.cpp
@@ -1,7 +1,40 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
+int longestPalindromeSubseq(string s) {
+    int n = s.length();
+    vector<vector<int>> dp(n, vector<int>(n, 0));
 
+    for (int i = 0; i < n; i++) {
+        dp[i][i] = 1;
+    }
+
+    for (int len = 2; len <= n; len++) {
+        for (int i = 0; i <= n - len; i++) {
+            int j = i + len - 1; 
+            
+            if (s[i] == s[j]) {
+                dp[i][j] = dp[i + 1][j - 1] + 2;
+            } else {
+                dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[0][n - 1];
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+    if (cin >> s) {
+        cout << longestPalindromeSubseq(s) << "\n";
+    }
+    return 0;
 }
