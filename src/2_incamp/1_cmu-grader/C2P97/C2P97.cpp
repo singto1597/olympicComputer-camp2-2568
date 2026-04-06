bb8ed2a8705d18682e7891b199aef9c22c8061ed
singto1597-cs-smu <singto1597@gmail.com>
2026-04-06 08:45:31 +0700

    add sheets

--- src/2_incamp/1_cmu-grader/C2P97/C2P97.cpp
+++ src/2_incamp/1_cmu-grader/C2P97/C2P97.cpp
@@ -2,6 +2,27 @@
 
 using namespace std;
 
+
+
 int main(){
-    
+    int t;
+    cin >> t;
+
+    for (int _ = 0; _ < t; _++){
+        int n, m, start_node, to_node;
+
+        vector<vector<pair<int, int>>> adj_list;
+        
+
+        for (int i = 0; i < m; i++){
+            int u, v, w;
+            cin >> u >> v >> w;
+
+            adj_list[u].push_back({v, w});
+            adj_list[v].push_back({u, w});
+
+        }
+
+
+    }
 }
