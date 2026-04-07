8ac76dfd752f7e4d1b1a16c668d3cf5caef67252
singto1597-cs-smu <singto1597@gmail.com>
2026-04-07 09:57:09 +0700

    LCS

--- src/2_incamp/1_cmu-grader/C2P60/C2P60.cpp
+++ src/2_incamp/1_cmu-grader/C2P60/C2P60.cpp
@@ -2,6 +2,42 @@
 
 using namespace std;
 
-int main(){
+vector<vector<int>> dp;
 
+int recur(string x, string y, int i, int j){
+    if (i == 0 || j == 0) return 0;
+    if (dp[i][j] != -1) return dp[i][j];
+    if (x[i - 1] == y[j - 1]){
+        return dp[i][j] = 1 + recur(x, y, i-1, j-1);
+    }
+    return dp[i][j] = max(recur(x, y, i - 1, j), recur(x, y, i, j - 1));
+}
+
+int bottom_up(string x, string y){
+    int m = x.length(), n = y.length();
+    for (int i = 1; i <= m; i++){
+        for(int j = 1; j <= n; j++){
+            if (x[i - 1] == y[j - 1]){
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+            else{
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[m][n];
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    string x, y;
+    cin >> x;
+    cin >> y;
+
+    // dp.assign(1005, vector<int>(1005, -1));
+    dp.assign(1005, vector<int>(1005, 0));
+
+
+    // cout << recur(x, y, x.size(), y.size());
+    cout << bottom_up(x, y);
 }
--- src/2_incamp/1_cmu-grader/C2P61/C2P61.cpp
+++ src/2_incamp/1_cmu-grader/C2P61/C2P61.cpp
@@ -2,6 +2,38 @@
 
 using namespace std;
 
+vector<int> arr;
+
+vector<int> increasing_subsequence;
+
+//LCS
+
 int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int n;
+    cin >> n;
+    if (n == 0){
+        cout << 0 << endl;
+        return 0;
+    }
+    arr.assign(n+5, 0);
+    increasing_subsequence.assign(n + 5, 1);
+
+    for (int i = 0; i < n; i ++){
+        cin >> arr[i];
+    }
+
+    int max_num = 1;
+
+    for (int i = 0; i < n; i ++){
+        for (int j = 0; j < i; j++){
+            if (arr[j] < arr[i]){
+                increasing_subsequence[i] = max(increasing_subsequence[j] + 1, increasing_subsequence[i]);
+                if (increasing_subsequence[i] > max_num) max_num = increasing_subsequence[i];
+            }
+        }
+    }
+
+    cout << max_num << endl;
 
 }
