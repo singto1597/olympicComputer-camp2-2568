03719b13e6dd50f2b5751f105c238b7141c25c04
singto1597-cs-smu <singto1597@gmail.com>
2026-04-07 15:42:40 +0700

    do quest

--- src/2_incamp/1_cmu-grader/C2P86/C2P86.cpp
+++ src/2_incamp/1_cmu-grader/C2P86/C2P86.cpp
@@ -2,6 +2,33 @@
 
 using namespace std;
 
-int main(){
+// long long f(int n){
+//     if (n == 0) return 1;
+//     if (n == 1) return 0;
 
+//     return f(n - 2) + g(n - 1) * 2;
+// }
+
+// long long g(int n){
+//     if (n == 0) return 0;
+//     if (n == 1) return 1;
+
+//     return f(n - 1) + g(n - 2);
+// }
+
+int main(){
+    int f[32] = {}, g[32] = {};
+    f[0] = g[1] = 1;
+    f[1] = g[0] = 0;
+    for (int n = 2; n <= 30; ++n) {
+        f[n] = 2 * g[n - 1] + f[n - 2];
+        g[n] = f[n - 1] + g[n - 2];
+    }
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++){
+        int n;
+        cin >> n;
+        cout << f[n] << endl;
+    }
 }
