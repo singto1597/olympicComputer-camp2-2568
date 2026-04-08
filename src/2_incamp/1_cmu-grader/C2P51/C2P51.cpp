9dbc1bb9119ae1052b6c51433e2679113089e592
singto1597-cs-smu <singto1597@gmail.com>
2026-04-08 10:11:27 +0700

    follow Instructor

--- src/2_incamp/1_cmu-grader/C2P51/C2P51.cpp
+++ src/2_incamp/1_cmu-grader/C2P51/C2P51.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
+
+
 int main(){
 
+    long long n, ans;
+    cin >> n;
+
+
+    long long arr[n + 5] = {};
+
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+        if (i == 0){
+            ans = max(arr[i], 0LL);
+        }
+        else{
+            long long max_end_at_i = max(arr[i], arr[i - 1] + arr[i]);
+            ans = max(max_end_at_i, ans);
+            arr[i] = max_end_at_i; // ไม่ใช่ input แล้ว
+        }
+    }
+    cout << ans << endl;
 }
--- src/2_incamp/1_cmu-grader/TASK_098/TASK_098.cpp
+++ src/2_incamp/1_cmu-grader/TASK_098/TASK_098.cpp
@@ -1,7 +1,40 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int n, m;
+    if (!(cin >> n >> m)) return 0;
+
+
+    vector<vector<int>> dp(n, vector<int>(m, 0));
+    int max_size = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            int val;
+            cin >> val;
+            
+            if (val == 1) {
+
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]}) + 1;
+                }
+                max_size = max(max_size, dp[i][j]);
+            } else {
+                dp[i][j] = 0; 
+            }
+        }
+    }
+
+    cout << max_size << "\n";
+
+    return 0;
 }
